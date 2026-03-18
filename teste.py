7d5dbb494f12966a115e26f18367185f64241594
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 20:46:52 -0300

    feat: logica de retirar a menor nota dos checkpoint

--- teste.py
+++ teste.py
@@ -15,27 +15,33 @@
 # gs22 = float(input("Global Solution: "))
 
 # semestre 1
-cp11 = 7
-cp12 = 1
+cps1 = [2, 4, 10]
 sp11 = 9
 sp12 = 1
 gs11 = 9
 
 # semestre 2
-cp21 = 4
-cp22 = 0
+cps2 = [5, 8, 10]
 sp21 = 5
 sp22 = 4
 gs22 = 10
 
-m1 = ((cp11 + cp12) / 2) * 0.2 + (sp11 + sp12) * 0.1 + gs11 * 0.6
-m2 = ((cp21 + cp22) / 2) * 0.2 + (sp21 + sp22) * 0.1 + gs22 * 0.6
+cps1.remove(min(cps1))
+cps2.remove(min(cps2))
+# print(f"Checkpoint Maiores {cps1}, {cps2}")
+
+cps1Soma = sum(cps1)
+cps2Soma = sum(cps2)
+# print(f"Checkpoint Soma, Semestre 1: {cps1Soma}, Semestre 2: {cps2Soma}")
+
+m1 = (cps1Soma / 2) * 0.2 + (sp11 + sp12) * 0.1 + gs11 * 0.6
+m2 = (cps2Soma / 2) * 0.2 + (sp21 + sp22) * 0.1 + gs22 * 0.6
 mf = m1 * 0.4 + m2 * 0.6
 
-if mf < 6:
-    re = f"Vc perdeu de ano, sua nota foi {mf}"
+if mf >= 6:
+    re = f"Parabens vc passou de ano, sua nota foi {mf} 👌"
 else:
-    re = f"Parabens vc passou de ano, sua nota foi {mf}"
+    re = f"Vc perdeu de ano, sua nota foi {mf}"
 
 print(" ")
 print(re)
