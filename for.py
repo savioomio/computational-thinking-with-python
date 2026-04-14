2f77fa7b8784584e44dd32d0560a818e0bf358f0
savioomio <saviopessoaafonso@gmail.com>
2026-04-13 21:27:53 -0300

    feat: for

---
+++ for.py
@@ -0,0 +1,41 @@
+# min = 10000
+# max = 0
+#
+# for i in range(1, 11):
+#     n = int(input(f"Me der Numero {i}° intero: "))
+#     if n > max:
+#         max = n
+#     elif n < min:
+#         min = n
+#
+#
+# print(f"O valor digitado foi maior e menor: {max} e {min}")
+#
+#
+# N = int(input("me der um numero para eu te dar o fatorial: "))
+#
+# while N == int or N <= 0 or N == 1:
+#     print("Precisa ser um numero valido para podemos te entregar o fatorial dele\n")
+#     N = int(input("me der um numero para eu te dar o fatorial: "))
+#
+# for i in range(N, 1, -1):
+#     fam = N
+#     fat = fam * (N-i)
+#     if N <= 1:
+#         print(f"\nO fatorial é {fat}")
+#
+# n1 = -1
+# n2 = -1
+# alunos = -1
+# notas = -1
+#
+# while alunos <= 0:
+#     alunos = int(input("Me der a quantidade de alunos da turma: "))
+#
+# while notas <= 0:
+#     notas = int(input("Me der a quantidade de notas de cada aluno: "))
+#
+# for Al in range(alunos):
+#     for Nt in range(notas):
+#        n1 = int(input(f"Nota {Nt}° do aluno {Al}: "))
+#        print(n1)
